7c4a20428fa7817a5104d3da186dcd48ad7457a0
Pivil <remi.pontonnier.38@gmail.com>
2020-04-03 11:28:51 +0200

    .

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const express = require("express");
 const app = express();
 const weather = require("./backend/weather.js");
-const steam = require("./backend/steam")
+const steam = require("./backend/steam");
 
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("templates/home");
 });
 
-app.get("/weather", function (req, res) {
+app.get("/weather", function(req, res) {
   weather.getWeather().then(data => {
     console.log("data => ", data);
     var temp = data.main.temp;
     var city = data.name;
-    // res.send("OLALA il fait " + temp + " degré à " + city);
-    res.render("test", { temp: temp });
+    res.render("weather", { temp: temp, city: city });
   });
 });
 
-
-
-app.get('/Steam', function (req, res) {
+app.get("/Steam", function(req, res) {
   steam.getSteam().then(data => {
     console.log("data => ", data);
-    var nick = data.nickname
-    var real = data.realName
-    res.send("Surnom: " + nick + "\nVrai nom: " + real);
+    var nick = data.nickname;
+    var real = data.realName;
+    // res.send("Surnom: " + nick + "\nVrai nom: " + real);
+    res.render("steam");
   });
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
 });
